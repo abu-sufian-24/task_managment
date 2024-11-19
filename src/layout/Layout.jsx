23e0ce9aa9fe1867a20b9468b00a438445b32f57
abu-sufian-24 <abusufian300400@gmail.com>
2024-11-19 11:25:10 +0600

    done

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,19 +2,21 @@ import Footer from "../sections/footer/Footer"
 import Header from "../sections/Header/Header"
 import Banner from "../sections/banner/Banner"
 import TaskTable from "../task-table/TaskTable"
-import Container from "../component/Container";
+
 
 
 
 
 function Layout() {
   return (
-    <Container>
+    <>
       <Header />
       <Banner />
       <TaskTable />
       <Footer />
-    </Container>
+    </>
+
+
   )
 }
 
